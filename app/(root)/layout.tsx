340a64b06c4dabc5c236c4d6df9b665cd2ef67c7
mohmmedabdelaal <medo.elaal@gmail.com>
2023-11-08 03:09:56 +0200

    global css added

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,15 +1,30 @@
-import Navbar from '../../components/Navbar';
+import {ClerkProvider} from "@clerk/nextjs";
+import  type {Metadata} from 'next';
 import Head from '../head';
+import '../globals.css'
 
+export const metadata: Metadata = {
+    title: 'DevOverflow',
+    description: 'Website about developers and needs for jobs demands which the market needs',
+    icons: {
+        icon: '../assets/images/default-logo.svg'
+    }
+}
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
+      <ClerkProvider>
+
     <html>
       <Head />
-      <body>{children}</body>
+
+      <body>
+      <h1>From the future</h1>
+      {children}</body>
     </html>
+      </ClerkProvider>
   );
 }
