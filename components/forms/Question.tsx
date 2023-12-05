4a4534308add30487fa3e1b8658f296b6185dff0
mohmmedabdelaal <medo.elaal@gmail.com>
2023-12-06 01:13:35 +0200

    form created with shadcn

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -1,6 +1,18 @@
 'use client';
 
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 import * as z from 'zod';
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
 
 const formSchema = z.object({
   username: z
