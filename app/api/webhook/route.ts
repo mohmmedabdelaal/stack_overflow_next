7a7ea058fba4b842240c6af598d4f6adefcf47a2
mohmmedabdelaal <medo.elaal@gmail.com>
2024-01-04 03:08:11 +0200

    edit secert key

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -7,7 +7,7 @@ import {NextResponse} from "next/server";
 export async function POST(req: Request) {
 
     // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-    const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
+    const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET
 
     if (!WEBHOOK_SECRET) {
         throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
@@ -52,6 +52,7 @@ export async function POST(req: Request) {
     // Get the ID and type
 
     const eventType = evt.type;
+    console.log({eventType})
 
    if(eventType === 'user.created'){
        // @ts-ignore
