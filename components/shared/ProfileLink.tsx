82f44b306dce469da93782b241642e2a1348f2f1
mohmmedabdelaal <medo.elaal@gmail.com>
2024-05-30 10:08:27 +0300

    statsCard added

--- components/shared/ProfileLink.tsx
+++ components/shared/ProfileLink.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import Link from "next/link";
-import Image from "next/image";
+import Link from 'next/link';
+import Image from 'next/image';
 
 interface ProfileProps {
-    imgUrl: string;
-    href?: string;
-    title: string;
+  imgUrl: string;
+  href?: string;
+  title: string;
 }
 
-const ProfileLink = ({imgUrl, href, title}: ProfileProps) => {
-    return (
-        <div className="flex-center gap-1">
-            <Image src={imgUrl} alt="icon" width={20} height={20}/>
-            {href ? (
-                <Link href={href} target="_blank" className="paragraph-meduim text-blue-500">
-                    {title}
-                </Link>
-            ) : (
-                <p className="paragraph-meduim text-dark400_light700">{title}</p>
-            )}
-        </div>
-    );
+const ProfileLink = ({ imgUrl, href, title }: ProfileProps) => {
+  return (
+    <div className="flex-center gap-1">
+      <Image src={imgUrl} alt="icon" width={20} height={20} />
+      {href ? (
+        <Link
+          href={href}
+          target="_blank"
+          className="paragraph-meduim text-blue-500"
+        >
+          {title}
+        </Link>
+      ) : (
+        <p className="paragraph-meduim text-dark400_light700">{title}</p>
+      )}
+    </div>
+  );
 };
 
 export default ProfileLink;
